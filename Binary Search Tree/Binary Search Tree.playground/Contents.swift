e5148af71088344a184dc2873d1d7119a6ad99b7
Kanwar Zorawar Singh Rana <roadtriptool@gmail.com>
2017-12-16 14:37:34 -0500

    Adding Search Element to BST

--- Binary Search Tree/Binary Search Tree.playground/Contents.swift
+++ Binary Search Tree/Binary Search Tree.playground/Contents.swift
@@ -31,6 +31,29 @@ func insert(rootNode node:Node?, withData data: Int)-> Node{
     return node
 }
 
+func search(rootNode node:Node?, forData data: Int)-> Bool{
+    
+    guard let node = node else {
+        print("No Data")
+        return false
+    }
+    
+    print("root Node: \(String(describing: node.data)) Searth: \(data)")
+    
+    if (data == node.data){
+        print("Found Data")
+        return true
+    }
+    else if (data <= node.data){
+        print("Left Child \(node.data)")
+        return search(rootNode: node.leftChild, forData: data)
+    }
+    else {
+        print("Right Child \(node.data)")
+        return search(rootNode: node.rightChild, forData: data)
+    }
+}
+
 var root: Node! = nil
 root = insert(rootNode: root, withData: 15)
 root = insert(rootNode: root, withData: 10)
@@ -40,3 +63,8 @@ root = insert(rootNode: root, withData: 3)
 root = insert(rootNode: root, withData: 12)
 root = insert(rootNode: root, withData: 25)
 
+print ("\nSearch Item\n")
+
+var itemPresent: Bool  = false
+itemPresent =  search(rootNode: root, forData: 0)
+print("Item Present: \(itemPresent)")
