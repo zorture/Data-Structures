4aaf8a0e88974c72e1d4d64f9e8ab69f21822cde
Kanwar Zorawar Singh Rana <roadtriptool@gmail.com>
2017-12-17 02:35:03 -0500

    BST Traversal Techniques
    Breadth-first
    	Level Order
    
    Depth-first
    	Pre Order
    	In Order
    	Post Order

--- Binary Search Tree/Binary Search Tree.playground/Contents.swift
+++ Binary Search Tree/Binary Search Tree.playground/Contents.swift
@@ -31,6 +31,15 @@ func insert(rootNode node:Node?, withData data: Int)-> Node{
     return node
 }
 
+var root: Node! = nil
+root = insert(rootNode: root, withData: 15)
+root = insert(rootNode: root, withData: 10)
+root = insert(rootNode: root, withData: 20)
+root = insert(rootNode: root, withData: 5)
+root = insert(rootNode: root, withData: 3)
+root = insert(rootNode: root, withData: 12)
+root = insert(rootNode: root, withData: 25)
+
 func search(rootNode node:Node?, forData data: Int)-> Bool{
     
     guard let node = node else {
@@ -54,6 +63,12 @@ func search(rootNode node:Node?, forData data: Int)-> Bool{
     }
 }
 
+print ("\nSearch Item\n")
+
+var itemPresent: Bool  = false
+itemPresent =  search(rootNode: root, forData: 0)
+print("Item Present: \(itemPresent)")
+
 func findMin(rootNode node:Node?)-> Int?{
     
     guard let node = node else {
@@ -66,6 +81,10 @@ func findMin(rootNode node:Node?)-> Int?{
     
     return findMin(rootNode: leftChild)
 }
+
+let minValue = findMin(rootNode: root)
+print("\nMin Item: \(String(describing: minValue))")
+
 
 func findMax(rootNode node:Node?)-> Int?{
     
@@ -80,6 +99,9 @@ func findMax(rootNode node:Node?)-> Int?{
     return findMax(rootNode: rightChild)
 }
 
+let maxValue = findMax(rootNode: root)
+print("\nMax Item: \(String(describing: maxValue))")
+
 func findMaxHeight(rootNode node: Node?)-> Int {
     
     guard let node = node else {
@@ -88,27 +110,98 @@ func findMaxHeight(rootNode node: Node?)-> Int {
     return max(findMaxHeight(rootNode: node.leftChild), findMaxHeight(rootNode: node.rightChild)) + 1
 }
 
-var root: Node! = nil
-root = insert(rootNode: root, withData: 15)
-root = insert(rootNode: root, withData: 10)
-root = insert(rootNode: root, withData: 20)
-root = insert(rootNode: root, withData: 5)
-root = insert(rootNode: root, withData: 3)
-root = insert(rootNode: root, withData: 12)
-root = insert(rootNode: root, withData: 25)
-
-print ("\nSearch Item\n")
-
-var itemPresent: Bool  = false
-itemPresent =  search(rootNode: root, forData: 0)
-print("Item Present: \(itemPresent)")
-
-let minValue = findMin(rootNode: root)
-print("\nMin Item: \(String(describing: minValue))")
-
-let maxValue = findMax(rootNode: root)
-print("\nMax Item: \(String(describing: maxValue))")
-
-
 let height = findMaxHeight(rootNode: root)
 print("\nHeight: \(height)")
+
+class Queue {
+    var collection = [Node]()
+    
+    func enQueue(withData data: Node){
+        collection.append(data)
+    }
+    
+    func deQueue() -> Node? {
+        return collection.removeFirst()
+    }
+    
+    func qSize() {
+        print(collection.count)
+    }
+    
+    func isEmpty() -> Bool {
+        return collection.count == 0 ? true : false
+    }
+}
+
+
+print ("\n Level Order Traversal\n")
+
+func levelOrderTraversal(rootNode node: Node?) {
+    
+    guard let node = node else {
+        print("Empty Tree")
+        return
+    }
+
+    let queue = Queue()
+    queue.enQueue(withData: node)
+    
+    while !queue.isEmpty() {
+        guard let  node = queue.deQueue()else{
+            return
+        }
+        print(node.data)
+        if let leftNode = node.leftChild {
+            queue.enQueue(withData: leftNode)
+        }
+        
+        if let rightNode = node.rightChild {
+            queue.enQueue(withData: rightNode)
+        }
+    }
+}
+
+levelOrderTraversal(rootNode: root)
+
+print ("\n Pre Order Traversal\n")
+
+func preOrderTraversal(rootNode node: Node?) {
+    
+    guard let node = node else {
+        return
+    }
+    print(node.data)
+    preOrderTraversal(rootNode: node.leftChild)
+    preOrderTraversal(rootNode: node.rightChild)
+    
+}
+
+preOrderTraversal(rootNode: root)
+
+print ("\n In Order Traversal\n")
+
+func inOrderTraversal(rootNode node: Node?) {
+    
+    guard let node = node else {
+        return
+    }
+    inOrderTraversal(rootNode: node.leftChild)
+    print(node.data)
+    inOrderTraversal(rootNode: node.rightChild)
+}
+
+inOrderTraversal(rootNode: root)
+
+print ("\n Post Order Traversal\n")
+
+func postOrderTraversal(rootNode node: Node?) {
+    
+    guard let node = node else {
+        return
+    }
+    postOrderTraversal(rootNode: node.leftChild)
+    postOrderTraversal(rootNode: node.rightChild)
+    print(node.data)
+}
+
+postOrderTraversal(rootNode: root)
