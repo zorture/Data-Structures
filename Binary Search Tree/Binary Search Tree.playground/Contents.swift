e215dfc77aba7b255e30ecc6330d0bff2c3e4e77
Kanwar Zorawar Singh Rana <roadtriptool@gmail.com>
2017-12-17 16:09:43 -0500

    Deleting element from BST

--- Binary Search Tree/Binary Search Tree.playground/Contents.swift
+++ Binary Search Tree/Binary Search Tree.playground/Contents.swift
@@ -36,9 +36,10 @@ root = insert(rootNode: root, withData: 15)
 root = insert(rootNode: root, withData: 10)
 root = insert(rootNode: root, withData: 20)
 root = insert(rootNode: root, withData: 5)
-root = insert(rootNode: root, withData: 3)
 root = insert(rootNode: root, withData: 12)
+root = insert(rootNode: root, withData: 17)
 root = insert(rootNode: root, withData: 25)
+root = insert(rootNode: root, withData: 3)
 root = insert(rootNode: root, withData: 7)
 root = insert(rootNode: root, withData: 11)
 root = insert(rootNode: root, withData: 14)
@@ -48,65 +49,65 @@ root = insert(rootNode: root, withData: 22)
 root = insert(rootNode: root, withData: 30)
 
 
-func search(rootNode node:Node?, forData data: Int)-> Bool{
+func search(rootNode node:Node?, forData data: Int)-> Node?{
     
     guard let node = node else {
-        //print("No Data")
-        return false
+        return nil
     }
     
     if (data == node.data){
-        //print("Found Data")
-        return true
+        return node
     }
     else if (data <= node.data){
-        //print("Left Child \(node.data)")
         return search(rootNode: node.leftChild, forData: data)
     }
     else {
-        //print("Right Child \(node.data)")
         return search(rootNode: node.rightChild, forData: data)
     }
 }
 
 print ("\nSearch Item\n")
 
-var itemPresent: Bool  = false
-itemPresent =  search(rootNode: root, forData: 0)
-print("Item Present: \(itemPresent)")
+if let node =  search(rootNode: root, forData: 0){
+    print("Item Present")
+}
+else{
+    print("Item Not Present")
+}
 
-func findMin(rootNode node:Node?)-> Int?{
+
+func findMin(rootNode node:Node?)-> Node?{
     
     guard let node = node else {
         return nil
     }
     
     guard let leftChild = node.leftChild else {
-        return node.data
+        return node
     }
     
     return findMin(rootNode: leftChild)
 }
 
 let minValue = findMin(rootNode: root)
-print("\nMin Item: \(String(describing: minValue))")
+print("\nMin Item: \(minValue?.data)")
 
 
-func findMax(rootNode node:Node?)-> Int?{
+func findMax(rootNode node:Node?)-> Node?{
     
     guard let node = node else {
         return nil
     }
     
     guard let rightChild = node.rightChild else {
-        return node.data
+        return node
     }
     
     return findMax(rootNode: rightChild)
 }
 
 let maxValue = findMax(rootNode: root)
-print("\nMax Item: \(String(describing: maxValue))")
+print("\nMax Item: \(maxValue?.data)")
 
 func findMaxHeight(rootNode node: Node?)-> Int {
     
@@ -148,7 +149,7 @@ func levelOrderTraversal(rootNode node: Node?) {
         //print("Empty Tree")
         return
     }
-
+    
     let queue = Queue()
     queue.enQueue(withData: node)
     
@@ -233,3 +234,53 @@ func isBinarySearchTree(rootNode node: Node?, lowerLimit: Int, uppperLimit: Int)
 
 let value = isBinarySearchTree(rootNode: root, lowerLimit:0, uppperLimit: 100)
 print(value)
+
+print ("\n Delete Node \n")
+
+func deleteNode(rootNode node: Node?, withData data: Int)-> Node? {
+    
+    guard var node = node else {
+        return nil
+    }
+    
+    if data < node.data {
+        node.leftChild = deleteNode(rootNode: node.leftChild, withData: data)
+    }else if data > node.data {
+        node.rightChild =  deleteNode(rootNode: node.rightChild, withData: data)
+    }else {
+        // Case 1 No Child
+        if node.leftChild == nil && node.rightChild == nil  {
+            return nil
+        }else {
+            //Case 2 one Chil
+            if node.leftChild == nil {
+                return node.rightChild
+            }else if node.rightChild == nil {
+                return node.leftChild
+            }else {
+                // Case 3 two Child
+                print("Got it")
+                guard let maxValue = findMax(rootNode: node.leftChild) else {
+                    return node
+                }
+                node.data = maxValue.data
+                node.leftChild = deleteNode(rootNode: node.leftChild, withData: maxValue.data)
+            }
+        }
+    }
+    return node
+}
+
+deleteNode(rootNode: root, withData: 17)
+print ("\n In-Order Traversal \n")
+levelOrderTraversal(rootNode: root)
+
+
+
+
+
+
+
+
+
+
