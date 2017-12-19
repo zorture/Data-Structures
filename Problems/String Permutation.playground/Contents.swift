57dbceae9a43a1f293ccf39d0de11366980dc4ec
Kanwar Zorawar Singh Rana <roadtriptool@gmail.com>
2017-12-18 22:21:54 -0500

    String Permutation Algorithm

---
+++ Problems/String Permutation.playground/Contents.swift
@@ -0,0 +1,45 @@
+//  Created by Kanwar Zorawar Singh Rana on 12/18/17.
+//  Copyright © 2017 Kanwar Zorawar Singh Rana. All rights reserved.
+
+func stringPermutation(charAr: [Character], countAr: [Int], result: [Character], level: Int){
+    
+    var result = result
+    var countAr = countAr
+    if level == charAr.count{
+        
+        let result = result.reduce("") {char1, char2 in "\(char1)\(char2)"}
+        print("\(result)")
+        return
+    }
+    
+    
+    for i in 0..<charAr.count{
+        if countAr[i] == 0{
+            continue
+        }
+        
+        result.append(charAr[i])
+        countAr[i] -= 1
+        stringPermutation(charAr: charAr, countAr: countAr, result: result, level: level + 1)
+        countAr[i] += 1
+        result.removeLast()
+    }
+    
+}
+
+let string = "AABC"
+
+let charArray = Array(string)
+let dict = [Character: Int]()
+
+let charDict = charArray.reduce(into: [:]) { counts, word in
+    counts[word, default: 0] += 1
+}
+
+let charAr = Array(charDict.keys)
+let countAr = Array(charDict.values)
+
+let result = [Character]()
+
+stringPermutation(charAr: charAr, countAr: countAr, result: result, level: 0)
+
